9b3a3ad73a67e6fa80cc4bc15b3aff9faac267d5
Grace Xu <gracexucs@gmail.com>
2026-01-11 15:15:44 -0600

    Data prep complete

--- R/config.R
+++ R/config.R
@@ -1,7 +1,7 @@
 SEED        <- 123
 
-DATA_RAW    <- "data/loan_default_raw.csv"
-DATA_MODEL  <- "data/loan_default_modeling.parquet"
+DATA_RAW    <- "data/raw.csv"
+DATA_MODEL  <- "data/modeling.rds"
 SPLIT_FILE  <- "data/split_obj.rds"
 
 DIR_FIGURES <- "figures"
